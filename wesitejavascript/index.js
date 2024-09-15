10ae0f451a7e6ad6db6aa858b985f76ba542f669
Sitherankan sinnappu <123910569+darkexcution@users.noreply.github.com>
2024-09-15 15:13:58 -0400

    Add files via upload

---
+++ wesitejavascript/index.js
@@ -0,0 +1,14 @@
+//console.log("hello");
+//console.log("i like pizza!");
+
+//window.alert("this is an alert!");
+//window.alert("i like a pizza");
+
+//document.getElementById("myH1").textContent = "hello";
+//document.getElementById("myP").textContent="i like pizza";
+
+//let x=123;
+//console.log(x);
+
+let age= 25;
+console.log("you are ${age} years old")
